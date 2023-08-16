cb5856ff6745f68cab077de899f51a4e1f5fbeae
K413l <kalel.schlichting@gmail.com>
2023-08-15 22:47:49 -0300

    mudancas

--- matrizes/matriz.py
+++ matrizes/matriz.py
@@ -1,7 +1,7 @@
 import Imagem as img
 
 nome = "naturezaMorta.jpg"
-dados = img.getMatrizImagemColorida(nome)
+dados = img.getMatrizImagemColorida("naturezaMorta.jpg")
 
 matrizR = dados[0]
 matrizG = dados[1]
@@ -10,3 +10,12 @@ matrizB = dados[2]
 lin = len(matrizR)
 col = len(matrizR[0])
 print(lin, "X", col)
+
+for i in range(lin):
+    for j in range(col):
+        if matrizR[i][j] == 10 and matrizG[i][j] == 30 and matrizB[i][j] == 91:
+            matrizR[i][j] == 255
+            matrizG[i][j] == 255
+            matrizB[i][j] == 255
+
+img.salvaImagemColorida("nat2.jpg", matrizR, matrizG, matrizB)
