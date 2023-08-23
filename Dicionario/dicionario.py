9bea9c704d047e93c9308fac7317944c955eb3a9
K413l <kalel.schlichting@gmail.com>
2023-08-22 21:56:32 -0300

    asdasd

---
+++ Dicionario/dicionario.py
@@ -0,0 +1,8 @@
+tabela = {}
+                    #pts, vitorias, gols marcados, gols contra
+tabela['botafogo'] = [16, 6, 34, 10]
+tabela['flamengo'] = [16, 6, 34, 10]
+tabela['corinthians'] = [16, 6, 34, 10]
+tabela['spfc'] = [16, 6, 34, 10]
+
+print(tabela)
